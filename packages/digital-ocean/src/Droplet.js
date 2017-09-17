cc44ad110bb453f3cb1539e2e273510fd202f0d2
Mark Miyashita <negativetwelve@gmail.com>
2017-09-17 04:18:28 -0700

    Add error catching to the Droplet command

--- packages/digital-ocean/src/Droplet.js
+++ packages/digital-ocean/src/Droplet.js
@@ -14,9 +14,17 @@ class Droplet {
     });
 
     const json = await response.json();
-    const {droplet} = json;
+    const {status} = response;
 
-    return new this(droplet);
+    if (status >= 200 && status < 300) {
+      return new this(json.droplet);
+    }
+
+    // If the request failed, we need to throw an error with the text.
+    const error = new Error(response.statusText);
+
+    error.response = response;
+    throw error;
   }
 
   // --------------------------------------------------
